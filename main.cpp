4c2139efe9c754ff2096cf09d59fedb148691d31
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2022-10-31 15:41:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,9 +14,9 @@ int main() {
 
   ASSERT(btree->toString(" ") == "11 36 45 47 70 75 100 111 114 120",
          "The function insert is not working");
-  ASSERT(btree->height() == 3, "The function height is not working");
+  ASSERT(btree->height() == 2, "The function height is not working");
   for (int i = 0; i < 10; i++) {
-    ASSERT(findKey(btree->search(numbers[i]), numbers[i]),
+    ASSERT(btree->search(numbers[i]) == true,
            "The function search is not working");
   }
   ASSERT(btree->minKey() == 11, "The function minKey is not working");
@@ -28,26 +28,26 @@ int main() {
 
   ASSERT(btree->toString(" ") == "11 36 47 70 75 114 120",
          "The function remove is not working");
-  ASSERT(btree->height() == 2, "The function height is not working");
+  ASSERT(btree->height() == 1, "The function height is not working");
 
   btree->remove(114);
   btree->insert(40);
   
   ASSERT(btree->toString(" - ") == "11 - 36 - 40 - 47 - 70 - 75 - 120",
          "The function toString is not working");
-  ASSERT(btree->height() == 3, "The function height is not working");  
+  ASSERT(btree->height() == 2, "The function height is not working");  
   
   btree->insert(125);
   btree->insert(115);
   
   ASSERT(btree->maxKey() == 125, "The function maxKey is not working");
-  ASSERT(btree->search(47) == true),  "The function search is not working");  
+  ASSERT(btree->search(47) == true,  "The function search is not working");  
   
   btree->remove(11);
     
-  ASSERT(btree->search(11) == false),  "The function search is not working");
+  ASSERT(btree->search(11) == false,  "The function search is not working");
   ASSERT(btree->minKey() == 36, "The function minKey is not working");
-  ASSERT(btree->size() == 7, "The function size is not working");
+  ASSERT(btree->size() == 8, "The function size is not working");
   
   btree->clear();  
   ASSERT(btree->size() == 0, "The function size is not working");
