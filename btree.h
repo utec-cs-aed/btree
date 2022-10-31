9bc69b163dfdcd040280c78b7eaf343c553a4678
Heider Sanchez <48141762+heidersanchez@users.noreply.github.com>
2022-10-31 08:18:03 -0500

    Update btree.h

--- btree.h
+++ btree.h
@@ -10,18 +10,21 @@ class BTree {
  private:
   Node<TK>* root;
   int M;  // grado u orden del arbol
+  int n; // total de elementos en el arbol 
 
  public:
   BTree(int _M) : root(nullptr), M(_M) {}
 
-  bool search(TK key);
-  void insert(TK key);
-  void remove(TK key);
-  int height();
-  string toString(const string& sep);  // inorder
+  bool search(TK key);//indica si se encuentra o no un elemento
+  void insert(TK key);//inserta un elemento
+  void remove(TK key);//elimina un elemento
+  int height();//altura del arbol. Considerar altura 0 para arbol vacio
+  string toString(const string& sep);  // recorrido inorder
 
   TK minKey();  // minimo valor de la llave en el arbol
   TK maxKey();  // maximo valor de la llave en el arbol
+  void clear(); // eliminar todos lo elementos del arbol
+  int size(); // retorna el total de elementos insertados  
   ~BTree();     // liberar memoria
 };
 
